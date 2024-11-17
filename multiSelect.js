19fcf0d78455cbfc45a4cceaab41e3cc3867d7f0
Gabriel Attal <gabriel.attal.pro@gmail.com>
2024-11-17 16:12:47 +0100

    fix

--- multiSelect.js
+++ multiSelect.js
@@ -170,7 +170,6 @@ export const MultiSelect = {
 
                 noneButtonElement.addEventListener('click', () => {
 
-                    console.log(jsonPayload);
                     window.voiceflow.chat.interact({
                         type: 'complete',
                         payload: JSON.stringify({
